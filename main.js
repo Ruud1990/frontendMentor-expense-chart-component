451529fac2048af908817f6f69d16814c1d9395d
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2023-02-27 22:52:26 +0100

    added final code

--- main.js
+++ main.js
@@ -2,17 +2,41 @@ import  mockData  from './data.json' assert { type: "json" };
 
 
 function days (){
-    return displayDay()
-}
+    window.addEventListener("DOMContentLoaded", function(){
+
+        displayDay()
+        showPopup()
+    }
+)}
 function displayDay (){
     const newData = mockData.mockData;
     console.log(newData);
     const daysContainer = document.querySelector('.days-container');
     let showDays = newData.map(function(newData) {
-        return `<div class="day-item"><h4>${newData.day}</h4> <div class="bar"></div></div>`
+        return `<div class="day-item"><div class="bar-popup">$${newData.amount}</div><div class="bar" style="height: ${newData.amount}px"></div><h4>${newData.day}</h4></div>`
+        
     })
+    
     showDays = showDays.join("");
     daysContainer.innerHTML = showDays;
 }
 
-days ();
+
+function showPopup(){
+    const dayBars = document.querySelectorAll('.bar');
+    const daypopup = document.querySelectorAll('.bar-popup');
+    console.log(dayBars);
+    console.log(daypopup);
+    for (let i = 0; i < dayBars.length; i++){
+        dayBars[i].addEventListener('mouseover', function(){
+            daypopup[i].classList.add('show')
+        })
+        dayBars[i].addEventListener('mouseleave', function(){
+            daypopup[i].classList.remove('show')
+        })
+    }
+
+    
+}
+
+days();
