b794b40f9d299368627cb0cfeacef73903eeeb68
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2023-02-28 10:34:19 +0100

    added total amount

--- main.js
+++ main.js
@@ -6,27 +6,35 @@ function days (){
 
         displayDay()
         showPopup()
+        totalAmount()
     }
-)}
-function displayDay (){
+    )}
     const newData = mockData.mockData;
-    console.log(newData);
-    const daysContainer = document.querySelector('.days-container');
-    let showDays = newData.map(function(newData) {
-        return `<div class="day-item"><div class="bar-popup">$${newData.amount}</div><div class="bar" style="height: ${newData.amount}px"></div><h4>${newData.day}</h4></div>`
-        
-    })
+
+    function displayDay (){
+        const daysContainer = document.querySelector('.days-container');
+        let showDays = newData.map(function(newData) {
+            return `<div class="day-item"><div class="bar-popup">$${newData.amount}</div><div class="bar" style="height: ${newData.amount}px"></div><h4>${newData.day}</h4></div>`
+            
+        })
     
     showDays = showDays.join("");
     daysContainer.innerHTML = showDays;
 }
 
+function totalAmount(){
+        const totalAmount = document.querySelector('.total-value');
+        const totalValue = newData.reduce(function(x, y) {
+            return x + y.amount
+        }, 0)
+        totalAmount.textContent = `$${totalValue}`;
+}
+
+
 
 function showPopup(){
     const dayBars = document.querySelectorAll('.bar');
     const daypopup = document.querySelectorAll('.bar-popup');
-    console.log(dayBars);
-    console.log(daypopup);
     for (let i = 0; i < dayBars.length; i++){
         dayBars[i].addEventListener('mouseover', function(){
             daypopup[i].classList.add('show')
